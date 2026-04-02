3a1bb82a5f218b6d9e9d9883ab5d046a8dfb713b
RoberthBG <roberthbg.2026@hotmail.com>
2026-04-01 22:16:30 -0500

    Implementación de la vista de de clientes

---
+++ Financiera.BusinessLogic/InyectorDependencias.cs
@@ -0,0 +1,21 @@
+﻿using Financiera.Data.Infrastructure;
+using Financiera.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financiera.BusinessLogic
+{
+    public static class InyectorDependencias
+    {
+        public static void Inyeccion(this IServiceCollection services)
+        {
+            services.AddScoped<ICliente, ClienteRepositorio>();
+            services.AddScoped<ITipoCliente, TipoClienteRepositorio>();
+            services.AddScoped<PrestamoServices>();
+        }
+    }
+}
